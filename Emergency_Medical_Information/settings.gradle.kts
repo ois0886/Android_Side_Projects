7ed087ff544a0e7eb05fb4c1ebb721f1794197ad
oh in seong <58154638+ois0886@users.noreply.github.com>
2024-05-26 15:35:12 +0900

    init setting

---
+++ Emergency_Medical_Information/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Emergency_Medical_Information"
+include(":app")
+ 
